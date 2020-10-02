143825ce55396970203111dcd3b130bf8b0555f0
Diablo <750108747@qq.com>
2020-10-02 10:23:39 +0800

    实现 aside 切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
 <div>
     <Topnav />
     <div class="content">
-        <aside>
+        <aside v-show="asideVisible">
             <h2>组件列表</h2>
             <ol>
                 <li>
@@ -25,10 +25,20 @@
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from '../components/Topnav.vue';
+import {
+    inject,
+    Ref
+} from 'vue';
 export default {
     components: {
         Topnav
+    },
+    setup() {
+        const asideVisible = inject < Ref < boolean >> ('asideVisible');
+        return {
+            asideVisible
+        }
     }
 }
 </script>
