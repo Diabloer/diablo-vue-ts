00275eede1818abf7e449644da8874c5b5d0e3fb
Diablo <750108747@qq.com>
2020-10-06 21:17:07 +0800

    style(doc): 整体文档css

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
-    Switch 组件
+    Switch 文档
+    <p v-for="item in 100" :key="item">Switch 的文档</p>
 </div>
 </template>
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -32,8 +32,13 @@ export default {
     background: pink;
     display: flex;
     padding: 16px;
-    position: relative;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
     z-index: 10;
+    justify-content: center;
+    align-items: center;
 
     >.logo {
         max-width: 6rem;
