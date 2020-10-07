1838d6c7ebeb0cc6f5d101fa37ae9a8a3f52d895
Diablo <750108747@qq.com>
2020-10-07 15:10:11 +0800

    feat(Switch): Switch 组件基本样式，hover动画

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,6 +1,14 @@
 <template>
 <div>
-    Switch 文档
-    <p v-for="item in 100" :key="item">Switch 的文档</p>
+    <Switch />
 </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components: {
+        Switch
+    }
+}
+</script>
