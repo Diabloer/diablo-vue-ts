59b634f8961ef858df7e45dbedd8586121c57268
Diablo <750108747@qq.com>
2020-11-15 21:28:21 +0800

    css(lib): 最小影响原则 1. 组件样式名基本格式 gulu-组件名__功能；

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-<button class="gulu-switch" :class="{checked:value}" @click="toggle"><span></span></button>
+<button class="gulu-switch" :class="{'gulu-switch__checked':value}" @click="toggle"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -48,11 +48,11 @@ $h2: $h - 4px;
         outline: none;
     }
 
-    &.checked {
+    &.gulu-switch__checked {
         background: #1890ff;
     }
 
-    &.checked>span {
+    &.gulu-switch__checked>span {
         left: calc(100% - #{$h2} - 2px);
     }
 
@@ -62,7 +62,7 @@ $h2: $h - 4px;
         }
     }
 
-    &.checked:active {
+    &.gulu-switch__checked:active {
         >span {
             width: $h2 + 4px;
             margin-left: -4px;
