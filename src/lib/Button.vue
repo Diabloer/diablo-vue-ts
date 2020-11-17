8b754e4b58dfa0970d86af2b932a71ff239d2e50
Diablo <750108747@qq.com>
2020-11-17 21:47:07 +0800

    style(Button): Button 组件实现 disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-<button class="gulu-button" :class="classes">
+<button class="gulu-button" :class="classes"  :disabled="disabled">
     <slot />
 </button>
 </template>
@@ -23,6 +23,10 @@ export default {
         level: {
             type: String,
             default: "normal"
+        },
+        disabled: {
+            type: Boolean,
+            default: false
         }
     },
     setup(props, context) {
@@ -46,6 +50,7 @@ $color: #333;
 $blue: #409EFF;
 $radius: 4px;
 $red: #F56C6C;
+$grey: grey;
 .gulu-button {
     box-sizing: border-box;
     height: $h;
@@ -146,6 +151,21 @@ $red: #F56C6C;
             &:focus {
                 color: darken($red, 10%);
             }
+        }
+    }
+    &.gulu-theme-button {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+            &:hover {
+                border-color: $grey;
+            }
+        }
+    }
+    &.gulu-theme-link, &.gulu-theme-text {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
         }
     }
 }  
