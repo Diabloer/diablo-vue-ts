34bf236c92432cdf576ef8901748a03628f9b4ba
Diablo <750108747@qq.com>
2020-11-17 21:26:19 +0800

    style(Button): Button组件的主题样式和大小样式 1. 主题：theme button link text； 2. 大小：size  big normal small；

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,11 @@
 <template>
-<button class="gulu-button"
-    :class="{[`gulu-theme-${theme}`]: theme}"
-><slot /></button>
+<button class="gulu-button" :class="classes">
+    <slot />
+</button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 export default {
     // 1. props 需要先声明才能取值，attrs 不用申明
     // 2. props 不包含事件，attrs 包含
@@ -14,10 +15,21 @@ export default {
         theme: {
             type: String,
             default: "button"
+        },
+        size: {
+            type: String,
+            default: "normal"
         }
     },
     setup(props, context) {
-        
+        const { theme, size } = props;
+        const classes = computed(() => {
+            return {
+                [`gulu-theme-${theme}`]: theme,
+                [`gulu-size-${size}`]: size
+            };
+        });
+        return { classes };
     }
 }
 </script>
@@ -55,6 +67,32 @@ $radius: 4px;
     }
     &::-moz-focus-inner {
         border: 0;
+    }
+    &.gulu-theme-link{
+        border-color: transparent;
+        box-shadow: none;
+        color: $blue;
+        &:hover,&:focus{
+            color: lighten($blue, 10%);
+        }
+    }
+    &.gulu-theme-text{
+        border-color: transparent;
+        box-shadow: none;
+        color: inherit;
+        &:hover,&:focus{
+            background: darken(white, 5%);;
+        }
+    }
+    &.gulu-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
+    }
+    &.gulu-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
     }
 }  
 </style>
